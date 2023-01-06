7b276e89a481307f68363bea7ae541e010ce66c5
Philip <philip.dan.taylor@gmail.com>
2023-01-06 20:17:44 +0000

    Add files for task '12. To uppercase'

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def uppercase(str):
+    capsDiff = ord('A') - ord('a')
+    l_str = list(str)
+    for i, c in enumerate(str):
+        if (ord(c) in range(ord('a'), ord('z'))):
+            l_str[i] = chr(ord(c) + capsDiff)
+    print("".join(l_str))
