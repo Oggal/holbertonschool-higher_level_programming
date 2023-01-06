75c4b44772c3196e87e85dc80f5774e188f23643
Philip <philip.dan.taylor@gmail.com>
2023-01-06 20:19:43 +0000

    Correct Logic for z

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,6 +5,6 @@ def uppercase(str):
     capsDiff = ord('A') - ord('a')
     l_str = list(str)
     for i, c in enumerate(str):
-        if (ord(c) in range(ord('a'), ord('z'))):
+        if (ord(c) in range(ord('a'), ord('z')+1)):
             l_str[i] = chr(ord(c) + capsDiff)
     print("".join(l_str).format())
