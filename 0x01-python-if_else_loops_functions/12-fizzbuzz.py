829838c7f318dcd9a1d56502b3cd7d9bb65c91bf
Philip <philip.dan.taylor@gmail.com>
2023-01-06 20:44:26 +0000

    Add Files for Task 14

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def fizzbuzz():
+    printtext = ""
+    for i in range(1,101):
+        printtext = ""
+        if (i % 3 == 0):
+            printtext += "Fizz"
+        if (i % 5 == 0):
+            printtext += "Buzz"
+        if (printtext == ""):
+            printtext = "{}".format(i)
+        print(printtext.format(), end=" ")
+
