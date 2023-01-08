37ef60e983b96229ea374470a8fcdda5a6fab02d
Philip <philip.dan.taylor@gmail.com>
2023-01-08 00:01:54 +0000

    PEP8
    I'll always miss having the semicolon

--- 0x01-python-import_modules/0-add.py
+++ 0x01-python-import_modules/0-add.py
@@ -3,8 +3,8 @@
 
 def main():
     from add_0 import add as adder
-    a = 1;
-    b = 2;
+    a = 1
+    b = 2
     print("{0} + {1} = {2}".format(a, b, adder(a, b)))
 
 if (__name__ == '__main__'):
