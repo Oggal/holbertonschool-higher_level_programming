04029edf5d374a4c1af07b9ee8db61e9d1f07289
Philip <philip.dan.taylor@gmail.com>
2023-01-12 22:38:55 +0000

    Correct Var Name

--- 0x02-python-data_structures/2-replace_in_list.py
+++ 0x02-python-data_structures/2-replace_in_list.py
@@ -3,5 +3,5 @@
 
 def replace_in_list(my_list, idx, element):
     if(idx >= 0 and len(my_list) > idx):
-        mylist[idx] = element
-    return mylist
+        my_list[idx] = element
+    return my_list
