c9875d1437a9b1f38029899f07bd59f79c3dc076
Philip <philip.dan.taylor@gmail.com>
2023-01-13 19:30:22 +0000

    Corret shabang

--- 0x03-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x03-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,6 @@
-#!/usr/bin/pythone3
+#!/usr/bin/python3
 
 
 def print_sorted_dictionary(a_dictionary):
-    for k in sorted(a_dictionary):
+    for k in sorted(a_dictionary.keys()):
         print("{0}: {1}".format(k, a_dictionary.get(k)))
