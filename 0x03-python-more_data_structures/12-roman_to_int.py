98e6f6c8414547d73281a205fe3a0a13e92d9e94
Philip <philip.dan.taylor@gmail.com>
2023-01-13 22:21:10 +0000

    pep8 task 12

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -2,16 +2,16 @@
 
 
 def roman_to_int(string_in):
-    convert = {'I' : 1, 'V' : 5, 'X' : 10, 'L' : 50, 'C' : 100, 'D' : 500, 'M' : 1000}
+    convert = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     runningSum = 0
     last = None
     if string_in is None:
         return 0
     for x in reversed(list(string_in)):
-        i = convert.get(x,0) #Should just do .get(x) and catch the exception, but... TODO:Throw and Catch Expection
-        if last is None or i>=last:
+        i = convert.get(x, 0)
+        if last is None or i >= last:
             runningSum += i
         else:
-            runningSum -=i
+            runningSum -= i
         last = i
     return runningSum
