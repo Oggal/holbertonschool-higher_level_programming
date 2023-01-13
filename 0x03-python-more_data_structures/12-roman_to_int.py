d23ac3531d6acff9e257356dc4e25c4abca7eb54
Philip <philip.dan.taylor@gmail.com>
2023-01-13 22:37:58 +0000

    Add isInstance

--- 0x03-python-more_data_structures/12-roman_to_int.py
+++ 0x03-python-more_data_structures/12-roman_to_int.py
@@ -5,7 +5,7 @@ def roman_to_int(string_in):
     convert = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     runningSum = 0
     last = None
-    if string_in is None:
+    if string_in is None or not isinstance(string_in,str):
         return 0
     for x in reversed(list(string_in.upper())):
         try:
