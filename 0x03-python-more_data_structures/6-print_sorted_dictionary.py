1dc684367d268f6533fe1d1143c1f0686fb2ccc7
Philip <philip.dan.taylor@gmail.com>
2023-01-13 19:28:21 +0000

    Correct prototype

--- 0x03-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x03-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,6 +1,6 @@
 #!/usr/bin/pythone3
 
 
-def print_sorted_dictionary(richard):
-    for k in sorted(richard):
-        print("{0}: {1}".format(k, richard.get(k)))
+def print_sorted_dictionary(a_dictionary):
+    for k in sorted(a_dictionary):
+        print("{0}: {1}".format(k, a_dictionary.get(k)))
