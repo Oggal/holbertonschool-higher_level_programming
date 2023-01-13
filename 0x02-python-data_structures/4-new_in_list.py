2ae0346f4073715f0ce50959dfd86b079fa36989
Philip <philip.dan.taylor@gmail.com>
2023-01-13 02:51:17 +0000

    Add files for task 4

--- 0x02-python-data_structures/4-new_in_list.py
+++ 0x02-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 
-def replace_in_list(my_list, idx, element):
+def new_in_list(my_list, idx, element):
     new_list = my_list.copy()
     if(idx >= 0 and len(new_list) > idx):
         new_list[idx] = element
