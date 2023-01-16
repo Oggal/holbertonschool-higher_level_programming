c6c79aa2772fbb5dfc28d7ce3c3e71afba7889ce
Philip <philip.dan.taylor@gmail.com>
2023-01-16 18:18:19 +0000

    removed a lot of docs`

--- 0x04-python-classes/3-square.py
+++ 0x04-python-classes/3-square.py
@@ -1,24 +1,15 @@
 #!/usr/bin/python3
-"""Simple Square Project
-
-This Module contains a class representing a simple Square.
-"""
+"""Simple Square Project, not over documented"""
 
 
 class Square:
     """Class representing a simple square"""
     @property
     def size(self):
-        """int: Size of a Simple Square
-        Value must be a positive integer.
-        """
         return self.__size
 
     @property.setter
     def size(self, value):
-        """int: Size of Simple Square
-        Value must be an int >=0 
-        """
         if (not isinstance(value, int)):
             raise TypeError("size must be an integer")
         if value < 0:
@@ -26,15 +17,8 @@ class Square:
         self.__size = value
 
     def __init__(self, _size=0):
-        """Initialize a simple Square
-        Args:
-            _size (int): Size of the Square.
-        """
+
         self.size = _size
 
     def area(self):
-        """Get Area of a Square
-        Returns:
-            int: Area of Square
-        """
         return self.__size ** 2
