9282967821040b4719fcd22d73ab189803781694
Philip <philip.dan.taylor@gmail.com>
2023-01-16 17:37:05 +0000

    Correct Task 3

--- 0x04-python-classes/3-square.py
+++ 0x04-python-classes/3-square.py
@@ -13,14 +13,15 @@ Example:
 class Square:
     """Class representing a simple square
     """
-    def size(self,value):
+    @property
+    def size(self, value):
         
         if (not isinstance(value, int)):
             raise TypeError("size must be an integer")
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-
+    @property.setter
     def size(self):
         """int: Size of a Simple Square
         Value must be a positive integer."""
