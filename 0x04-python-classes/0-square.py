87521f7a9a626f1e169bce9e65e370fd4049db8a
Philip <philip.dan.taylor@gmail.com>
2023-01-16 17:00:49 +0000

    Correct Pep8

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -8,15 +8,13 @@ Example:
 
 """
 
+
 class Square:
     """Class representing a simple square
-    
     """
     def __init__(self, _size):
         """Initialize a simple Square
-        
         Args:
             _size (int): Size of the Square.
-        
         """
         self.__size = _size
