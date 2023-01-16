17736f07db70502686525436e4863bc4c59a2d85
Philip <philip.dan.taylor@gmail.com>
2023-01-16 16:51:41 +0000

    Expand Documentation of Square Class

--- 0x04-python-classes/0-square.py
+++ 0x04-python-classes/0-square.py
@@ -2,6 +2,14 @@
 
 
 class Square:
-    """Class representing a simple square"""
+    """Class representing a simple square
+    
+    """
     def __init__(self, _size):
+        """Initialize a simple Square
+        
+        Args:
+            _size (int): Size of the Square.
+        
+        """
         self.__size = _size
