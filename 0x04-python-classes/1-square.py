52cf452c1476322e4561698e0d460c249d4d69b4
Philip <philip.dan.taylor@gmail.com>
2023-01-16 17:07:47 +0000

    Add Files for task 1

---
+++ 0x04-python-classes/1-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Simple Square Project
+
+This Module contains a class representing a simple Square.
+
+Example:
+    >>>square = __import__('1-square').Square
+    >>>new_square = square(2)
+
+"""
+
+
+class Square:
+    """Class representing a simple square
+    """
+    def __init__(self, _size:int=0):
+        """Initialize a simple Square
+        Args:
+            _size (int): Size of the Square.
+        """
+        if _size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = _size
