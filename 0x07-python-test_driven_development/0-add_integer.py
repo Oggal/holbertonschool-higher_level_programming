bb2d179a0bb747498709ea5e717ee4922b5c65ec
Philip <philip.dan.taylor@gmail.com>
2023-01-19 20:53:40 +0000

    Adds a few more tests, Changes add_integer prototype

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -2,7 +2,7 @@
 """Task 0 - Project 0x05:: Add Integer"""
 
 
-def add_integer(a: int, b: int=98)->int:
+def add_integer(a, b=98)->int:
     """Adds two ints together"""
     a = a if isinstance(a, int) else int(a) if isinstance(a, float) else None
     b = b if isinstance(b, int) else int(b) if isinstance(b, float) else None
