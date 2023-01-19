c6e5c5bdb21ee53e87e098885e3b43e2fb395bdd
Philip <philip.dan.taylor@gmail.com>
2023-01-19 19:35:23 +0000

    Corretions for task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
+"""Task 0 - Project 0x05:: Add Integer"""
 
-def add_integer(a:int ,b:int=98)->int:
-    a = a if isinstance(a,int) else int(a) if isinstance(a, float) else None
-    b = b if isinstance(b,int) else int(b) if isinstance(b, float) else None
-    
-    if a== None:
+
+def add_integer(a: int, b: int=98)->int:
+    a = a if isinstance(a, int) else int(a) if isinstance(a, float) else None
+    b = b if isinstance(b, int) else int(b) if isinstance(b, float) else None
+    if a is None:
         raise TypeError("a must be an integer")
-    if b == None:
+    if b is None:
         raise TypeError("b must be an integer")
-    
     return a+b
