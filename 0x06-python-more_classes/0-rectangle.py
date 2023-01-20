df93ece700d5ae9a3c705845f34741d45fedfa19
Philip <philip.dan.taylor@gmail.com>
2023-01-20 23:02:57 +0000

    Correct Pep8

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -7,6 +7,7 @@ class Rectangle:
     @property
     def width(self):
         return self.__width
+    
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -14,9 +15,11 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width=value
+
     @property
     def height(self):
         return self.__height
+
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
@@ -24,6 +27,7 @@ class Rectangle:
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height=value
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
