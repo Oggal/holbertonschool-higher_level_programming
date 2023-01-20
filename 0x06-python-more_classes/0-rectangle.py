3777c4b392ac7bb9a70880235fc27ca32c30bc6f
Philip <philip.dan.taylor@gmail.com>
2023-01-20 23:04:11 +0000

    Correct Pep8 task 0

--- 0x06-python-more_classes/0-rectangle.py
+++ 0x06-python-more_classes/0-rectangle.py
@@ -7,14 +7,14 @@ class Rectangle:
     @property
     def width(self):
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self.__width=value
+        self.__width = value
 
     @property
     def height(self):
@@ -26,7 +26,7 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self.__height=value
+        self.__height = value
 
     def __init__(self, width=0, height=0):
         self.width = width
