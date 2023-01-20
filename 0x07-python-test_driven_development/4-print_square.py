e2e62a997b5a0b69c9bfe284db60d1f51aedff3c
Philip <philip.dan.taylor@gmail.com>
2023-01-20 21:55:09 +0000

    pep8 task 4

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+"""4-print_squre - Print a square"""
 
 
 def print_square(size):
+    """print_square - print a square with size number of # per side"""
     if not isinstance(size, (int)):
         if isinstance(size, float) and size > 0:
             size = int(size)
@@ -10,4 +12,4 @@ def print_square(size):
     if size < 0:
         raise ValueError("size must be >= 0")
     for x in range(size):
-       print("{}".format('#' * size))
+        print("{}".format('#' * size))
