fa724f09847cb3f920b84cc5d5ac2f06b6025eb7
Philip <philip.dan.taylor@gmail.com>
2023-01-20 16:47:49 +0000

    Update Task 1

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -2,10 +2,14 @@
 """Task 1 - Divide items in a matrix"""
 
 
+def bail():
+    raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+
+
 def matrix_divided(matrix, div):
     """Divide items in matrix by div"""
     if not (isinstance(div, int) or isinstance(div, float)):
-        raise TypeError()
+        raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError('division by zero')
     last = None
@@ -13,6 +17,7 @@ def matrix_divided(matrix, div):
     for row in matrix:
         if last is not None and last != len(row):
             raise TypeError("Each row of the matrix must have the same size")
-        newMatrix.append([round(x/div,2) for x in row])
+        newMatrix.append([round(x/div,2)  if isinstance(x, (int, float)) else bail() for x in row])
         last = len(row)
     return newMatrix
+
