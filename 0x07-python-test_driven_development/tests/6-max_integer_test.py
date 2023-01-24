1c704647ae7de840f0965288dea38559b2a81280
Philip <philip.dan.taylor@gmail.com>
2023-01-24 17:58:31 +0000

    Add More Tests to task 6

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -12,5 +12,7 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(max_integer([1.5, 5.5]), 5.5)
         self.assertEqual(max_integer("dogz"), 'z')
         self.assertEqual(max_integer([]), None)
+    def test_typeFails(self):
         self.assertRaises(TypeError, max_integer, None)
-        
+        self.assertRaises(TypeError, max_integer, [0, 5, None])
+        self.assertRaises(TypeError, max_integer, [0, 5, "0"])
