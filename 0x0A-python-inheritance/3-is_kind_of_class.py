e9021537df1a414b6577be361670a87fc47b4f80
Philip <philip.dan.taylor@gmail.com>
2023-01-26 17:28:27 +0000

    Add task 4

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Project 0x07 Task 2"""
+
+
 def is_kind_of_class(obj, a_class):
     """Good code documents itself"""
-    return isinstance(obj,a_class)
+    return isinstance(obj, a_class)
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -2,6 +2,6 @@
 """Project 0x07 Task 2"""
 
 
-def is_same_class(obj, a_class):
+def inherits_from(obj, a_class):
     """Good code documents itself"""
-    return (type(obj) is a_class)
+    return issubclass(obj, a_class)
