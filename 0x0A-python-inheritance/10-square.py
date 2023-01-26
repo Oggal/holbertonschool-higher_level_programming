f2e4550a5f80293c04fe7fd18fc76722e0da096f
Philip <philip.dan.taylor@gmail.com>
2023-01-26 19:52:11 +0000

    Correct Task 10

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """Project 0x07 Task 2"""
-Base = __import__('9-rectangle').Rectangle
+Rectangle = __import__('9-rectangle').Rectangle
 
 
-class Square(Base):
+class Square(Rectangle):
     """ The Square Squared Square Squares"""
     def __init__(self, size):
         super().__init__(size, size)
