a2f359e28e50ec43a5b6d7f7dc0dd9a150c79b3b
Philip <philip.dan.taylor@gmail.com>
2023-01-26 16:36:48 +0000

    Further modification of the function to include a large number of fields

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -4,4 +4,8 @@
 
 def lookup(obj):
     '''List the object attributes'''
-    return list(obj.__dict__.keys())
+    startlist = list(obj.__dict__.keys())
+    startlist.extend(x for x in dir(obj) if x not in startlist)
+    startlist.sort()
+    return startlist
+    
