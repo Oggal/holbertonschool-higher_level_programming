87ed570580d0f53574adae9e3aa5ae190ae48e1d
Philip <philip.dan.taylor@gmail.com>
2023-01-26 18:47:55 +0000

    Task 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Project 0x07 Task 2"""
+
+
+class BaseGeometry():
+    """base geometry classs"""
+    def area(self):
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        if not isinstance(value,int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
