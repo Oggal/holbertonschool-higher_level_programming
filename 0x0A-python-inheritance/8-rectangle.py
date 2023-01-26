76b12f0acaf9c943e7e97273e8eea977406cd859
Philip <philip.dan.taylor@gmail.com>
2023-01-26 19:40:25 +0000

    Add Task 8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Project 0x07 Task 2"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle Class Bro"""
+    def __init__(self,width, height) -> None:
+        super().__init__()
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)   
+        self.__width = width
+        self.__height = height
