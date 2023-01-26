f16730de50c0862cf0071fee31b97ef2257d9e8d
Philip <philip.dan.taylor@gmail.com>
2023-01-26 17:26:10 +0000

    orrect method name

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 """Project 0x07 Task 2"""
-def is_same_class(obj, a_class):
+def is_kind_of_class(obj, a_class):
     """Good code documents itself"""
     return isinstance(obj,a_class)
