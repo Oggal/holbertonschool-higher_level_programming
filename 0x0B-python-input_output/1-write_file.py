8bb97de550ae9fc4f59dcf4f737ad2d5d4737d3d
Philip <philip.dan.taylor@gmail.com>
2023-01-27 05:11:50 +0000

    Add return value

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,4 +5,5 @@
 def write_file(filename="", text=""):
     """Open up the file, loop the lines, and print as we loop"""
     with open(filename, 'w', encoding="utf-8") as f:
-        f.write(text)
+        i = f.write(text)
+    return i
