23db04f0152a19e645c9f70e455592d365f43694
Philip <philip.dan.taylor@gmail.com>
2023-01-29 02:52:49 +0000

    Update Task 1

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -7,10 +7,11 @@ class Base():
     __nb_objects = 0
 
     def __init__(self, id=None):
-        Base.__nb_objects += 1
+        if id is None:
+            Base.__nb_objects += 1
         self.id = Base.__nb_objects if id is None else id
 
-    def check_int(value=None):
+    def check_int(cls, value=None):
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         return value
