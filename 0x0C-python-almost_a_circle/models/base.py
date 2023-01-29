1f659f87155f876a7aa0c83a4f48fc4b3df98b83
Philip <philip.dan.taylor@gmail.com>
2023-01-29 02:02:32 +0000

    pep8 models.base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,6 +5,7 @@
 class Base():
     """Base Class"""
     __nb_objects = 0
+
     def __init__(self, id=None):
         Base.__nb_objects += 1
         self.id = Base.__nb_objects if id is None else id
