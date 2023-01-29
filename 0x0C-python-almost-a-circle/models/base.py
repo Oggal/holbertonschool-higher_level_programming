8368b9376b596010e8bda011717e8d5fe2bf54f8
Philip <philip.dan.taylor@gmail.com>
2023-01-29 01:43:53 +0000

    Task 1, Project 0x09, Almost a circle

---
+++ 0x0C-python-almost-a-circle/models/base.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Base Class"""
+
+
+class Base():
+    """Base Class"""
+    __nb_objects = 0
+    def __init__(self, id=None):
+        Base.__nb_objects += 1
+        self.id = Base.__nb_objects if id is None else id
