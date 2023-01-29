39d557ff22164e5eb9f75bd380f8c317b28a84da
Philip <philip.dan.taylor@gmail.com>
2023-01-29 18:52:14 +0000

    Add display func to rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -49,3 +49,12 @@ class Rectangle(Base):
     def area(self):
         """Calculate Area of rect"""
         return self.width * self.height
+
+    def display(self):
+        """Draw a rectangle"""
+        #placeholder for Y offset
+        for y_index in range(self.height):
+            for x_index in range(self.width):
+                #placeholder for x offset
+                print("#", end="")
+            print()
