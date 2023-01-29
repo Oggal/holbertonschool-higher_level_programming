80184f6dd4e4a8854cad81577b390cbd9a67a128
Philip <philip.dan.taylor@gmail.com>
2023-01-29 02:48:04 +0000

    Add Simple Test for Task 2

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Base Class Test Cases"""
+import unittest
+from models.rectangle import Rectangle
+
+
+class TestFib(unittest.TestCase):
+
+    def test_construct(self):
+        self.obj = Rectangle(1,1)
+        self.assertEqual(self.obj.height,1)
+        self.assertEqual(self.obj.width,1)
