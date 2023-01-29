154d615125b15571ecb6b3d1318646a8575fc604
Philip <philip.dan.taylor@gmail.com>
2023-01-29 02:22:46 +0000

    Add Task 2 and 3

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -9,3 +9,8 @@ class Base():
     def __init__(self, id=None):
         Base.__nb_objects += 1
         self.id = Base.__nb_objects if id is None else id
+
+    def check_int(value=None):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        return value
---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""Rectangle Class"""
+
+Base = __import__("base").Base
+
+
+
+class Rectangle(Base):
+    """Rectangle Class"""
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = Base.check_int(value)
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = Base.check_int(value)
+
+    @property
+    def x(self):
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        self.__x = Base.check_int(value)
+
+    @property
+    def y(self):
+        return self.__y
+
+    @y.setter
+    def height(self, value):
+        self.__y = Base.check_int(value)
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
