8e876792b29f866d373dee3b207617fbc608cb1f
Philip <philip.dan.taylor@gmail.com>
2023-01-30 16:30:50 +0000

    Simplfy logic for to_json_string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -26,6 +26,4 @@ class Base():
 
     def to_json_string(list_dictionaries):
         """get JSON strings"""
-        if list_dictionaries is None or list_dictionaries == []:
-            return "[]"
-        return json.dumps(list_dictionaries)
+        return json.dumps(list_dictionaries or [])
