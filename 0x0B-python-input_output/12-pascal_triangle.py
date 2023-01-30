91bc243f47b00a0b4be4481a04a2d57374536d5e
Philip <philip.dan.taylor@gmail.com>
2023-01-30 21:03:09 +0000

    Task 12

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""pascals triangle"""
+
+
+def pascal_triangle(n):
+    tris = []
+    prev = None
+    for i in range(n):
+        cur_layer = []
+        prev_r = range(i)
+        for j in range(i+1):
+            if prev is None:
+                cur_layer.append(1)
+            else:
+                a = (prev[j] if 0 <= j < len(prev) else 0)
+                b = (prev[j - 1] if 0 <= j-1 < len(prev) else 0)
+                cur_layer.append(a + b)
+        tris.append(cur_layer)
+        prev = tris[j]
+    return tris
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) == 2:
+        print(pascal_triangle(int(sys.argv[1])))
