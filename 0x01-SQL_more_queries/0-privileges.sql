81e3cac76e1ebf34f53a241e7b91fb5fb15b9483
Oggal <Philip.dan.taylor@gmail.com>
2023-02-10 10:33:47 -0800

    Task 0

--- 0x01-SQL_more_queries/0-privileges.sql
+++ 0x01-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- Display privliages for 2 users
-SHOW GRANTS FOR `user_0d_1 @ localhost`;
-ShOW GRANTS FOR `user_0d_2 @ localhost`;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+ShOW GRANTS FOR 'user_0d_2'@'localhost';
