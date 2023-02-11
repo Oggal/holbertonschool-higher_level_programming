20363a95043c3edda184617b23d9f339ece0535e
Oggal <Philip.dan.taylor@gmail.com>
2023-02-11 06:03:27 -0800

    Task 1, remove with grant

--- 0x01-SQL_more_queries/1-create_user.sql
+++ 0x01-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- Create a User for me
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVLIAGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVLIAGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVLIAGES;
