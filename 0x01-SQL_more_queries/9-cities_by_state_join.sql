21da259312712918240c0d0ec4c0b54a77a1d906
Oggal <Philip.dan.taylor@gmail.com>
2023-02-12 18:03:20 -0800

    Task 9, but with one select

--- 0x01-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x01-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- Select cities from cities where statie_id = (select id from states where name = 'california')
-SELECT id, name, (Select name from states WHERE id = state_id) as name FROM cities;
+SELECT cities.id, cities.name, states.name FROM cities INNER JOIN states ON state_id = states.id;
