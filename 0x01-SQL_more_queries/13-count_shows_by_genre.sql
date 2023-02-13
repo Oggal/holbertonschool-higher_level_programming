fb37c3f5c8d884f0c0f0ba2c73ab1fd0aa1673dd
Oggal <Philip.dan.taylor@gmail.com>
2023-02-13 09:03:15 -0800

    Better 13

--- 0x01-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x01-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,2 +1,2 @@
 -- Count By Genre
-SELECT tv_genres.name,count(*) AS number_of_shows from tv_genres left outer join tv_show_genres on tv_genres.id = tv_show_genres.genre_id GROUP BY tv_genres.name ORDER BY number_of_shows DESC;
+SELECT tv_genres.name as genre,count(*) AS number_of_shows FROM tv_genres LEFT OUTER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id GROUP BY tv_genres.name ORDER BY number_of_shows DESC;
