69fe2d7b7436ba73737a82922c23b3cd90a29c2f
Oggal <Philip.dan.taylor@gmail.com>
2023-02-13 09:58:00 -0800

    Correct order

--- 0x01-SQL_more_queries/16-shows_by_genre.sql
+++ 0x01-SQL_more_queries/16-shows_by_genre.sql
@@ -1,2 +1,2 @@
 -- Get Genres of Dexter
-SELECT title, name FROM tv_shows LEFT OUTER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id LEFT OUTER JOIN tv_genres ON genre_id = tv_genres.id ORDER BY title ASC;
+SELECT title, name FROM tv_shows LEFT OUTER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id LEFT OUTER JOIN tv_genres ON genre_id = tv_genres.id ORDER BY title, name ASC;
