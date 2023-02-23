6a48f95eaf875ec7d54140a0ccd6b95f2ee16228
Oggal <Philip.dan.taylor@gmail.com>
2023-02-23 14:27:20 -0800

    Task 0, Project 0x0B

---
+++ 0x0B-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+'''Get All States, requires User, Password, Database'''
+
+
+def get_all_states():
+    if(len(sys.argv) < 4):
+        print(sys.argv)
+        return
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    print("{} : {} @ {}".format(mysql_username,mysql_password,database_name))
+    try:
+        db = MySQLdb.connect("localhost",mysql_username,mysql_password,database_name)
+    except:
+        print("Failed")
+        return (0)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cursor.fetchall()
+    for r in rows:
+        print(r)
+    cursor.close()
+    db.close()
+
+if __name__ == '__main__':
+    get_all_states()
---
+++ 0x0B-python-object_relational_mapping/README.md
@@ -0,0 +1,3 @@
+## Project 0x0B 1161 Python - Object Relational Mapping
+
+holberton school
