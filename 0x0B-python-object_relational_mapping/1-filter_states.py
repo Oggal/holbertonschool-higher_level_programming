fc1a8239b5f85e7db343f49895e6600d50ea6329
Oggal <Philip.dan.taylor@gmail.com>
2023-02-23 17:44:35 -0800

    Task 1

---
+++ 0x0B-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+'''Get All States, requires User, Password, Database'''
+import MySQLdb
+import sys
+
+
+def get_all_states():
+    '''Get States from sql table'''
+    if(len(sys.argv) < 4):
+        print(sys.argv)
+        return
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    try:
+        db = MySQLdb.connect(
+            "localhost",
+            mysql_username,
+            mysql_password,
+            database_name)
+    except Exception as e:
+        return (0)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;")
+    rows = cursor.fetchall()
+    for r in rows:
+        print(r)
+    cursor.close()
+    db.close()
+
+
+if __name__ == '__main__':
+    get_all_states()
