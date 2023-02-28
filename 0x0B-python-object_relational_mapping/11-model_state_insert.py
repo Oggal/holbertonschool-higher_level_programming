0d64aafe9625c71bc811188a5efbc656bc5b74b9
Oggal <Philip.dan.taylor@gmail.com>
2023-02-28 07:05:16 -0800

    Task 11

---
+++ 0x0B-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""Fetch All States"""
+import sys
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+def insert_into_states():
+    '''Fetch All States'''
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+
+    target = sys.argv[4]
+    target = target.split('"')[0]
+    target = target.split("'")[0]
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = State(name="Louisiana")
+    session.add(state)
+    session.commit()
+    print("{}".format(state.id))
+
+
+if __name__ == "__main__":
+    insert_into_states()
