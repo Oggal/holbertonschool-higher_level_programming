03ee85dfd614b0d18de6f5bf7e3b14401a1d7927
Oggal <Philip.dan.taylor@gmail.com>
2023-02-28 05:28:42 -0800

    Task 10

---
+++ 0x0B-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Fetch All States"""
+import sys
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+def fetch_all_states():
+    '''Fetch All States'''
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+
+    target = sys.argv[4]
+    target = target.split('"')[0]
+    target = target.split("'")[0]
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).filter(
+        State.name.contains(target)).order_by(State.id).first()
+    if(state is None):
+        print("Not Found")
+        return
+    print("{}".format(state.id))
+
+
+if __name__ == "__main__":
+    fetch_all_states()
