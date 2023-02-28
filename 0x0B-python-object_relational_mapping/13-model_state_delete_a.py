42cc05cbfc8ad42100df7655497cfd2dc45ceba6
Oggal <Philip.dan.taylor@gmail.com>
2023-02-28 07:56:55 -0800

    Task 13

---
+++ 0x0B-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Fetch All States"""
+import sys
+from model_state import Base, State
+
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+def fetch_all_states():
+    '''Fetch All States'''
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for state in session.query(State).filter(
+     State.name.contains('a')).order_by(State.id).all():
+        session.delete(state)
+    session.commit()
+
+
+if __name__ == "__main__":
+    fetch_all_states()
