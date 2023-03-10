bae41fabf4bd85804987e4e6a2642916dc30f44a
Oggal <Philip.dan.taylor@gmail.com>
2023-03-10 10:35:55 -0800

    Add Task 9

---
+++ 0x0C-python-network_1/10-my_github.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Authenticates to Github API and displays user id"""
+import requests
+import sys
+
+
+def GetGithubID(usr, pswd):
+    """Authenticates to Github API and displays user id"""
+    url = 'https://api.github.com/user'
+    r = requests.get(url, auth=(usr, pswd))
+    try:
+        print(r.json().get('id'))
+    except:
+        print("None")
+
+if __name__ == "__main__":
+    if (len(sys.argv) == 3):
+        GetGithubID(sys.argv[1], sys.argv[2])
