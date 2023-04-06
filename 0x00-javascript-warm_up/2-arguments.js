0d2545bbe93bd3fc20dd01f6a1885f7263fd0f5e
Oggal <Philip.dan.taylor@gmail.com>
2023-04-06 12:57:48 -0700

    Remove extra consol.log

--- 0x00-javascript-warm_up/2-arguments.js
+++ 0x00-javascript-warm_up/2-arguments.js
@@ -5,7 +5,6 @@ const { argv } = require( 'node:process');
 
 let argc = argv.length
 const values = ['No argument', 'Argument found', 'Arguments found'];
-console.log(argc);
 argc = values[Math.min((argc - 2), 2)];
 
 console.log(argc);
