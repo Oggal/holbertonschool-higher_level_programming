cba6b34abc5c12130abf1d0ab5350a1cb1acc95e
Oggal <Philip.dan.taylor@gmail.com>
2023-04-07 06:40:03 -0700

    Correct Task 2

--- 0x00-javascript-warm_up/2-arguments.js
+++ 0x00-javascript-warm_up/2-arguments.js
@@ -1,12 +1,6 @@
 #!/usr/bin/node
 // Task 2, arguments
 
-const { argv } = require('node:process');
-
-const argc = argv.length;
+const argc = process.argv.length;
 const values = ['No argument', 'Argument found', 'Arguments found'];
-if (argc === 3) {
-  console.log(values[argc - 2]);
-} else {
-  console.log(values[argc - 2]);
-}
+console.log(values[Math.max(argc - 2, 2)]);
