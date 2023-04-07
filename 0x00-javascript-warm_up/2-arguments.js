f7f44416708fc35177a8dd0fb34aed0a81274d25
Oggal <Philip.dan.taylor@gmail.com>
2023-04-07 06:41:13 -0700

    Math correction

--- 0x00-javascript-warm_up/2-arguments.js
+++ 0x00-javascript-warm_up/2-arguments.js
@@ -3,4 +3,4 @@
 
 const argc = process.argv.length;
 const values = ['No argument', 'Argument found', 'Arguments found'];
-console.log(values[Math.max(argc - 2, 2)]);
+console.log(values[Math.min(argc - 2, 2)]);
