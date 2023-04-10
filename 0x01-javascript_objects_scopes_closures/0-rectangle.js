f6aea7098b5d89152ce9c2444fc592b3aebadf42
Oggal <Philip.dan.taylor@gmail.com>
2023-04-10 09:54:08 -0700

    Correct Export

--- 0x01-javascript_objects_scopes_closures/0-rectangle.js
+++ 0x01-javascript_objects_scopes_closures/0-rectangle.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
 // Task 0. Rectangle #0
 
-export class Rectangle {
-}
+module.exports = class Rectangle {};
--- 0x01-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x01-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 // Task 1. Rectangle #1
 
-export class Rectangle {
+module.exports = class Rectangle {
   constructor (w = 0, h = 0) {
     this.width = w;
     this.height = h;
   }
-}
+};
