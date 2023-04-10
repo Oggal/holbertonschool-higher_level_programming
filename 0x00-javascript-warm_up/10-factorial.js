aef7d1ba25697ba5730c361194b735f2e72dbd69
Oggal <Philip.dan.taylor@gmail.com>
2023-04-09 21:23:17 -0700

    Task 10

---
+++ 0x00-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// Task 10, x!
+
+const x = parseInt(process.argv[2]);
+
+function factorial (a) {
+  if (a === 1 || isNaN(a)) {
+    return 1;
+  }
+  return a * factorial(a - 1);
+}
+
+console.log(factorial(x));
