a0782430f503f787f71e7a7b8a178545164fbfb6
Oggal <Philip.dan.taylor@gmail.com>
2023-04-10 10:55:09 -0700

    Task 4

---
+++ 0x01-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,29 @@
+#!/usr/bin/node
+// Task 1. Rectangle #1
+
+module.exports = class Rectangle {
+  constructor (w = -1, h = -1) {
+    if (w <= 0 || h <= 0) {
+      return;
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
